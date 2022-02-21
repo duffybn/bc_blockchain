08d821b83a8d02bffa5d617df2345a4210e4b5af
Glitch (glitch-hello-react) <none>
2022-02-21 04:37:25 +0000

    💧💗 Checkpoint ./src/app.jsx:58163/31

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,13 @@ import React from "react";
 // Import and apply CSS stylesheet
 import "./styles/styles.css";
 
-
-
 // The component that adds our Meta tags to the page
 import Seo from './components/seo.jsx';
 
 // Home function that is reflected across the site
 export default function Home() {
   return (
-    
+    <>
       <Seo />
       <main role="main" className="wrapper">
         <div className="content">
@@ -19,7 +17,6 @@ export default function Home() {
           
         </div>
       </main>
-      
-    </Router>
+    </>
   );
 }
