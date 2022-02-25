a9bbda8e53d0c70b03bda880e3c83ccd00100e88
Glitch (glitch-hello-react) <none>
2022-02-25 14:45:03 +0000

    🍜👳 Checkpoint ./src/app.jsx:58163/57 ./src/components/Navbar.jsx:58163/268 ./src/components/Home.jsx:58163/126

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-// Import and apply CSS stylesheet
+// Import and apply global CSS stylesheet
 import "./styles/styles.css";
+// Import and apply App specific css
+import css from "./styles/App.module.css"
 
 // The component that adds our Meta tags to the page
 import Seo from './components/seo.jsx';
@@ -13,8 +15,9 @@ export default function Home() {
       <Seo />
       <div className={css.container}>
         <main role="main" className="wrapper">
-          Boston College React Template         
+          Content   
         </main>
+        
       </div>
     </>
   );
