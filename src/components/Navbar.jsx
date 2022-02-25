2628e5642a1a52379a6067eb59f279b2ecc39ee5
Glitch (glitch-hello-react) <none>
2022-02-25 15:20:00 +0000

    👆🎰 Checkpoint ./jsconfig.json:58163/73 ./src/components/Navbar.jsx:58163/318 ./src/styles/Navbar.module.css:58163/232 ./src/styles/styles.css:58163/82

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,15 @@
 
 import React from "react";
-import css from "styles/Navbar.module.css";
+import css from "../styles/Navbar.module.css";
 
 function Navbar() {
-  return <nav className={css.navBar}>
-    <div className={css.navItem}>
-      <button>
+  return <nav className={css.navbar}>
+    <div className={css.navitem}>
         <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/home.svg?v=1645800816915"/>
-      </button>
     </div>
-    <div className={css.navItem}>
-    </div>
-    <div className={css.navItem}>
-    </div>
-    
+    <div className={css.navitem}>
+      <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/newpost.svg?v=1645800817387"/>
+    </div>    
   </nav>;
 }
 
