0698025b6194509cf65a0bf1b2daf83b4fd6247b
Glitch (glitch-hello-react) <none>
2022-02-25 15:54:02 +0000

    🌟👗 Checkpoint ./src/components/Navbar.jsx:58163/580 ./src/styles/styles.css:58163/264 ./src/styles/Navbar.module.css:58163/40

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,12 @@ import React from "react";
 import css from "../styles/Navbar.module.css";
 
 function Navbar() {
+  
+  function handleClick(){
+    console.log("clicked!");
+  }
   return <nav className={css.navbar}>
-    <div className={css.navitem}>
+    <div className={css.navitem} onClick={handleClick}>
         <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/home.svg?v=1645800816915"/>
     </div>
     <div className={css.navitem}>
