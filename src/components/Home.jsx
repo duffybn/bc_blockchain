425fd99b67dcf06e8d448833b777fa2d8e5bdf8c
Glitch (glitch-hello-react) <none>
2022-03-13 05:11:50 +0000

    🍱🏦 Checkpoint ./src/components/Home.jsx:58163/2589 ./src/components/Post.jsx:58163/809 ./src/styles/Post.module.css:58163/108

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,26 @@
 import React from "react";
-
+import Post from "./Post.jsx";
 function Home() {
-  return <div>Home</div>;
+  const post = {
+    user: {
+      id: "judy",
+    },
+    post: {
+      id: "post-1",
+      userId: "judy",
+      photo:
+        "https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/post1.png?v=1645800818011",
+    },
+    likes: {
+      self: true,
+      count: 1,
+    },
+  };
+  return (
+    <div>
+      <Post user={post.user} likes={post.likes} post={post.post} />
+    </div>
+  );
 }
-
 
 export default Home;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import css from "../styles/Post.module.css";
 
-function Home() {
-  return <div>Home</div>;
+function Post(props) {
+  const user = props.user;
+  const post = props.post;
+  const likes = props.likes;
+  
+  return <div>
+    <div className={css.user}>@{user.id}</div>
+    <img className={css.photo} width="100%" src={post.photo}></img>
+    <div className={css.like}>
+      <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/like.svg?v=1645800817056"></img>
+    </div>
+    
+  </div>;
 }
 
 
-export default Home;
+export default Post;
