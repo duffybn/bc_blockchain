598d123a314e7a26fecbaffbb3782f092952afce
Glitch (glitch-hello-react) <none>
2022-03-13 04:47:49 +0000

    👐🎄 Checkpoint ./src/app.jsx:58163/1247 ./src/components/NewPost.js:58163/130

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./components/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
+import NewPost from "./components/NewPost.jsx";
 // Import and apply global CSS stylesheet
 import "./styles/styles.css";
 // Import and apply App specific css
@@ -11,18 +12,34 @@ import Seo from './components/seo.jsx';
 
 // App function that is reflected across the site
 export default class App extends React.Component{
-  
-  
-  return (
-    <>
-      <Seo />
-      <div className={css.container}>
-        <main role="main" className="wrapper">
-          <Home/>   
-        </main>
-        <Navbar/>
-        
-      </div>
-    </>
-  );
+  constructor(props){
+    this.state = {
+      page: "home"
+    }
+    super(props);
+  }
+  setPage(page){
+    this.setState({ page: page});
+  }
+  renderMain(){
+    switch(page){
+      case "home":  return <Home/>;
+      case "add": return <NewPost/>
+    }
+  }
+  render() {
+      return (    
+        <>
+          <Seo />
+          <div className={css.container}>
+            <main role="main" className="wrapper">
+              <Home/>   
+            </main>
+            <Navbar/>
+
+          </div>
+        </>
+      );
+  }
+
 }
