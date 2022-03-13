e78a181f76c3cb74a68350da547ab6ac2de7d903
Glitch (glitch-hello-react) <none>
2022-03-13 05:15:53 +0000

    🏉🍟 Checkpoint ./src/components/Post.jsx:58163/41

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,7 @@ function Post(props) {
   return <div>
     <div className={css.user}>@{user.id}</div>
     <img className={css.photo} width="100%" src={post.photo}></img>
-    <div className={css.like}>
+    <div className={css.like} >
       <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/like.svg?v=1645800817056"></img>
     </div>
     
