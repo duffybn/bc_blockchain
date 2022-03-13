d37fdf9918a1be5a371e7b0950e2ef173aa79d09
Glitch (glitch-hello-react) <none>
2022-03-13 04:59:49 +0000

    💒😼 Checkpoint ./src/app.jsx:58163/2462 ./src/components/Navbar.jsx:58163/85 ./src/components/Post.jsx:58163/97

--- src/app.jsx
+++ src/app.jsx
@@ -5,41 +5,42 @@ import NewPost from "./components/NewPost.jsx";
 // Import and apply global CSS stylesheet
 import "./styles/styles.css";
 // Import and apply App specific css
-import css from "./styles/App.module.css"
+import css from "./styles/App.module.css";
 
 // The component that adds our Meta tags to the page
-import Seo from './components/seo.jsx';
+import Seo from "./components/seo.jsx";
 
 // App function that is reflected across the site
-export default class App extends React.Component{
-  constructor(props){
-    this.state = {
-      page: "home"
-    }
+export default class App extends React.Component {
+  constructor(props) {
     super(props);
+    this.state = {
+      page: "home",
+    };
+    this.setPage = this.setPage.bind(this);
   }
-  setPage(page){
-    this.setState({ page: page});
+  setPage(page) {
+    this.setState({ page: page });
   }
-  renderMain(){
-    switch(page){
-      case "home":  return <Home/>;
-      case "add": return <NewPost/>
+  renderMain(page) {
+    switch (page) {
+      case "home":
+        return <Home />;
+      case "add":
+        return <NewPost />;
     }
   }
   render() {
-      return (    
-        <>
-          <Seo />
-          <div className={css.container}>
-            <main role="main" className="wrapper">
-              <Home/>   
-            </main>
-            <Navbar/>
-
-          </div>
-        </>
-      );
+    return (
+      <>
+        <Seo />
+        <div className={css.container}>
+          <main role="main" className="wrapper">
+            {this.renderMain(this.state.page)}
+          </main>
+          <Navbar onNavChange={this.setPage} />
+        </div>
+      </>
+    );
   }
-
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,17 @@
 import React from "react";
 import css from "../styles/Navbar.module.css";
 
-function Navbar() {
+function Navbar(props) {
   
   function handleClick(page){
     console.log("page clicked!", page);
+    props.onNavChange(page)
   }
   return <nav className={css.navbar}>
-    <div className={css.navitem} onClick={e=>handleClick("Home")}>
+    <div className={css.navitem} onClick={e=>handleClick("home")}>
         <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/home.svg?v=1645800816915"/>
     </div>
-    <div className={css.navitem} onClick={e=>handleClick("Add")}>
+    <div className={css.navitem} onClick={e=>handleClick("add")}>
       <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/newpost.svg?v=1645800817387"/>
     </div>    
   </nav>;
