2278af4a7d7df0dfbb1baa4fc3e9041b99db696d
Glitch (glitch-hello-react) <none>
2022-04-14 14:55:37 +0000

    🖼🐥 Checkpoint ./src/contexts/StoreContext.jsx:58163/1813

--- src/contexts/StoreContext.jsx
+++ src/contexts/StoreContext.jsx
@@ -4,14 +4,39 @@ import initialStore from "../utils/initialStore.js";
 export const StoreContext = createContext();
 
 export default function StoreContextProvider(props) {
+  // initialStore
   const [currentUserId, setCurrentUserId] = useState(
+    JSON.parse(window.localStorage.getItem("currentUserId")) ||    
     initialStore.currentUserId
   );
-  const [users, setUsers] = useState(initialStore.users);
-  const [posts, setPosts] = useState(initialStore.posts);
-  const [likes, setLikes] = useState(initialStore.likes);
-  const [comments, setComments] = useState(initialStore.comments);
-  const [followers, setFollowers] = useState(initialStore.followers);
+  useEffect(() => {
+    window.localStorage.setItem("currentUserId", JSON.stringify(currentUserId));
+  }, [currentUserId]);
+  
+  const [users, setUsers] = useState(JSON.parse(window.localStorage.getItem("users")) || initialStore.users);
+    useEffect(() => {
+    window.localStorage.setItem("users", JSON.stringify(users));
+  }, [users]);
+  
+  const [posts, setPosts] = useState(JSON.parse(window.localStorage.getItem("posts")) ||initialStore.posts);
+    useEffect(() => {
+    window.localStorage.setItem("posts", JSON.stringify(posts));
+  }, [posts]);
+  
+  const [likes, setLikes] = useState(JSON.parse(window.localStorage.getItem("likes")) ||initialStore.likes);
+  useEffect(() => {
+    window.localStorage.setItem("likes", JSON.stringify(likes));
+  }, [likes]);
+  
+  const [comments, setComments] = useState(JSON.parse(window.localStorage.getItem("comments")) ||initialStore.comments);
+  useEffect(() => {
+    window.localStorage.setItem("comments", JSON.stringify(comments));
+  }, [comments]);
+  
+  const [followers, setFollowers] = useState(JSON.parse(window.localStorage.getItem("followers")) ||initialStore.followers);
+  useEffect(() => {
+    window.localStorage.setItem("followers", JSON.stringify(followers));
+  }, [followers]);
 
   function addLike(postId) {
     const like = {
