ebd1af9747e5bc9cf7ee4ff6303732146d70cd15
Glitch (blockchain-test-2) <none>
2023-03-20 22:22:29 +0000

    🐝🙌 Checkpoint ./src/components/Header.jsx:41444162/670 ./src/components/Profile.jsx:41444162/222

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,11 @@ function Header() {
   return (
     <nav className={css.header}>
       <div className={css.headitem}>
+           <Link to="/newpost">
+              <img src="https://cdn.glitch.global/ce7a503b-7720-4b27-9a59-4a32173ba09d/newpost.svg?v=1647444622487" alt="NewPost"/>
+          </Link>
+      </div>
+      <div className={css.headitem}>
           <Link to="/">
               <img src="https://cdn.glitch.global/ce7a503b-7720-4b27-9a59-4a32173ba09d/home.svg?v=1647444623987" alt="Home"/>
           </Link>
@@ -21,6 +26,11 @@ function Header() {
       <div className={css.headitem}>
            <Link to="/newpost">
               <img src="https://cdn.glitch.global/ce7a503b-7720-4b27-9a59-4a32173ba09d/newpost.svg?v=1647444622487" alt="NewPost"/>
+          </Link>
+      </div>
+      <div className={css.headitem}>
+           <Link to="/profile">
+              <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/profile.svg?v=1645800817556" alt="NewPost"/>
           </Link>
       </div>
     </nav>
