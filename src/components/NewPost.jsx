ea4af4f6e69b45c5975427208382e4938eeac8ef
Glitch (blockchain-test-2) <none>
2023-03-20 22:45:24 +0000

    🎷⛴ Checkpoint ./src/components/NewPost.jsx:41444162/3512 ./src/styles/newPost.module.css:41444162/684

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import css from '../styles/newPost.module.css';
+import {StoreContext} from '../contexts/StoreContext.jsx';
 import {
   useNavigate,
-  useParams
+  useParams, 
+  Navigate
 } from "react-router-dom";
-function NewPost() {
+
+
+function NewPost(props) {
+  let {addPost} = useContext(StoreContext);
+  const [desc, setDesc] = useState('');
+  const [dragging, setDragging] = useState(false);
+  const [error, setError] = useState('');
+  const [photo, setPhoto] = useState(null);
   const navigate = useNavigate();
   const {param} = useParams();
   function handleClick(){
@@ -16,6 +26,93 @@ function NewPost() {
       <div>{param}</div>
     </div>
   );
+  
+   
+  function handleFileDragEnter(e){
+    setDragging(true);
+  }
+  function handleFileDragLeave(e){
+    setDragging(false);
+  }
+  function handleFileDrop(e){
+    if (e.dataTransfer.types.includes('Files')===false){
+      console.log("FAIL");
+      return;
+    }
+    if (e.dataTransfer.files.length>=1){
+      console.log("Success");
+      let file = e.dataTransfer.files[0];
+      if (file.size>1000000){
+        console.log("TOO BIG");
+        return;
+      }
+      if (file.type.match(/image.*/)){
+        let reader = new FileReader();
+        reader.onloadend = (e) => {
+          setPhoto(e.target.result)
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+    setDragging(false);
+  }
+  
+  function handleDescChange(e){
+    {console.log("DESC CHANGE")};
+    {console.log(desc)};
+    setDesc(e.target.value);
+  }
+  function handleSubmit(e){
+    if (photo){
+      setError(false);
+      addPost(photo, desc);
+      navigate("/");
+    }else{
+      setError(true);
+    }
+    event.preventDefault();
+  }
+  
+  function handleCancel(e){
+    {console.log("handleCancel")};
+    //cancelPost();
+    navigate(-1);
+  }
+  
+    return(
+    !props.currentUserId? <Navigate to="/login"/>:<div className={css.newPost}>
+      
+      <div className={css.photo}>
+        {!photo? <div className={css.message}>Drop your image</div>:
+                <img src={photo} alt="New Post"/>}
+          <FileLoader
+            onDragEnter={handleFileDragEnter}
+            onDragLeave={handleFileDragLeave}
+            onDrop={handleFileDrop}
+          >
+            <div className={[css.dropArea, dragging?css.dragging:null].join('')}
+            ></div>
+        </FileLoader>
+      
+      </div>
+      
+      <div className={css.desc} >
+        <textarea value={desc} onChange={handleDescChange} placeholder="Write your caption here!"/>
+        
+      </div>
+      
+      <div className={css.error}>
+        {error? <div>You need to add a photo!</div> : <div></div>}
+      </div>
+      
+      <div className={css.actions}>
+        <button onClick={handleCancel}>Cancel</button>
+        <button onClick={handleSubmit}>Share</button>
+      </div>
+      
+    </div>
+  );
+  
 }
 
 export default NewPost;
