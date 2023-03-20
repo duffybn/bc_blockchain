4b9b043ee6b4ac38db75ca5fe235a9b09f7224c7
Glitch (blockchain-test-2) <none>
2023-03-20 22:34:14 +0000

    🚿🐘 Checkpoint ./src/components/Header.jsx:41444162/624

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,14 @@ import {
 function Header() {
   return (
     <nav className={css.header}>
-      <div className={css.headitem}>
-           <Link to="/newpost">
-              <img src="https://cdn.glitch.global/ce7a503b-7720-4b27-9a59-4a32173ba09d/newpost.svg?v=1647444622487" alt="NewPost"/>
+       <div className={css.headitem}>
+          <Link to="/">
+              <img src="https://cdn.glitch.global/ce7a503b-7720-4b27-9a59-4a32173ba09d/home.svg?v=1647444623987" alt="Home"/>
           </Link>
       </div>
       <div className={css.headitem}>
-          <Link to="/">
-              <img src="https://cdn.glitch.global/ce7a503b-7720-4b27-9a59-4a32173ba09d/home.svg?v=1647444623987" alt="Home"/>
+           <Link to="/">
+              <img src="https://cdn.glitch.global/d7674480-73e0-49e0-acd4-3805874f347b/explore.svg?v=1645800816800" alt="NewPost"/>
           </Link>
       </div>
       <div>
