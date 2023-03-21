6d008cde20d9470fe3b656ec7def55006b3f64fe
Glitch (blockchain-test-2) <none>
2023-03-21 22:35:47 +0000

    🕍🔮 Checkpoint ./src/components/Profile.jsx:41444162/227 ./src/app.jsx:41444162/11

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useState, useEffect, useContext } from "react";
+import { useParams } from 'react-router-dom';
+import {
+  Link
+} from "react-router-dom";
+import {Navigate} from 'react-router-dom';
 
 function Profile(){
+  const {userId} = useParams();
     return(
     <div>
       Profile
