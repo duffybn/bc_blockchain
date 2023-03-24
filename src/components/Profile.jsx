4a708cf9c1d2fe87ae8e2a2f759121c2bb8d0fe2
Glitch (blockchain-test-2) <none>
2023-03-24 17:19:51 +0000

    ☔️🌍 Checkpoint ./src/components/Profile.jsx:41444162/366

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { useState, useEffect, useContext } from "react";
 import { useParams } from 'react-router-dom';
 import {
-  Link
+  Link,
+  
 } from "react-router-dom";
 import {Navigate} from 'react-router-dom';
+import initialStore from "../utils/initialStore.js"
 
 function Profile(){
   const {userId} = useParams();
+  const [filtered, setFiltered] = useState("")
+  const placeholder = "Jill"
+  
+  
+  function sort(data) {
+    const test = data.filter(d=> (d.userId == placeholder));
+    setFiltered(test);
+  }
     return(
     <div>
       Profile
-
+      {initialStore.map((post) => (
+          <div>{post.id}</div>
+        ))}
     </div>
   );
 }
