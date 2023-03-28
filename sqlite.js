957400d11d206af3e59f8d4c9411a23049a4a8a6
Glitch (blockchain-test-2) <none>
2023-03-28 16:00:25 +0000

    🍖🐔 Checkpoint ./sqlite.js:56164138/1082

--- sqlite.js
+++ sqlite.js
@@ -7,3 +7,31 @@ const dbWrapper = require("sqlite");
 const casual = require("casual");
 let db;
 
+dbWrapper
+  .open({
+    filename: dbFile,
+    driver: sqlite3.Database
+  })
+  .then(async dBase => {
+    db = dBase;
+
+    try {
+      if (!exists) {
+        await db.run(
+          "CREATE TABLE PROFESSORS (id INTEGER PRIMARY KEY AUTOINCREMENT, NAME VARCHAR(100) NOT NULL, Contract_address INTEGER NOT NULL UNIQUE)"
+        );
+        
+        await db.run(
+        "CREATE TABLE REVIEWS (id INTEGER PRIMARY KEY AUTOINCREMENT, contract INTEGER references PROFESSORS(contract), UNIVERSITY VARCHAR(100) NOT NULL, rating INTEGER  CHECK ( rating >= 0 AND rating <= 10), review VARCHAR(100))"
+        );
+        for (let r = 0; r < 5; r++)
+          await db.run(
+            "INSERT INTO Messages (message) VALUES (?)",
+            casual.catch_phrase
+          );
+      }
+      console.log(await db.all("SELECT * from Messages"));
+    } catch (dbError) {
+      console.error(dbError);
+    }
+  });
