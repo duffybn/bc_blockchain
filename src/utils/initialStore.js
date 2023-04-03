fd5448f0ffc7f3d5a79d323cfd34bc59f5e1b4df
Glitch (blockchain-test-2) <none>
2023-04-03 03:13:42 +0000

    🏓😘 Checkpoint ./src/utils/initialStore.js:41444162/100

--- src/utils/initialStore.js
+++ src/utils/initialStore.js
@@ -12,25 +12,21 @@ const initialStore = [
             grade: "6.5"
         },
         {
-            id:"post-3",
             userId:"Bob",
             desc: "he was ok",
             grade: "8.5"
         },
         {
-            id:"post-4",
             userId:"Jill",
             desc: "I loved this professor",
             grade: "10"
         },
         {
-            id:"post-5",
             userId: "Bob",
             desc: "Hated this man",
             grade: "1.5" 
         },
         {
-            id:"post-6",
             userId:"Jill",
             desc: "God awful",
             grade: "0.5"
