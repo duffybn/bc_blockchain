3d3284dd4f8262baf4cfb46e47a8e753cf74b40a
Glitch (blockchain-test-2) <none>
2023-04-03 21:13:55 +0000

    🕵🍇 Checkpoint ./src/utils/posts.js:41444162/67

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -2,7 +2,6 @@
 const posts = [{id: "1", professor: "Bob", college: "Boston College"}, 
                {id: "2", professor: "Jill", college: "Boston College"},
                {id: "3", professor: "Bill", college: "Boston College"},
-               {id: "4", professor: "Bill", college: "Boston Uni"}
               ];
 
 export default posts;
